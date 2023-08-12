1930d802812c9fe026d671b785a224d06333bdf3
Sandor Merle <sandorme@ob-i5.nsk.21-school.ru>
2023-08-12 17:30:58 +0700

    1

---
+++ src/list/list_iterators.h
@@ -0,0 +1,51 @@
+#ifndef CPP2_S21_LIST_LIST_ITERATORS_H_
+#define CPP2_S21_LIST_LIST_ITERATORS_H_
+
+namespace s21{
+
+template <class T>
+class ListIterator{
+friend class list<T>;
+
+public:
+	ListIterator() : ptr_(nullptr){};
+	ListIterator(Node *ptr) : ptr_(ptr){};
+
+	ListIterator &operator++(){
+		ptr_ = ptr_->next_;
+		return *this;
+	}
+	ListIterator &operator--(){
+		ptr_ = ptr_->prior_;
+		return *this;
+	}
+	ListIterator operator++(int){
+		ListIterator tmp = this*;
+		ptr_ = ptr_->next_;
+		return tmp;
+	}
+	ListIterator operator--(int){
+		ListIterator tmp = this *;
+		ptr_ = ptr_->prior_;
+		return tmp;
+	}
+	bool operator==(ListIterator other){
+		return ptr_ == other.ptr_;
+	}
+	bool operator!=(ListIterator other){
+		return ptr_ != other.ptr_;
+	}
+
+private:
+	Node *ptr_;
+	
+};
+
+template <class T>
+class ListConstIterator : public ListIterator<T>{
+
+};
+
+} // namespace s21
+
+#endif // CPP2_S21_LIST_LIST_ITERATORS_H_
