d090b37281bc5921968715053eff909913159aa9
Sandor Merle <sandorme@ob-i5.nsk.21-school.ru>
2023-08-12 18:47:24 +0700

    2

--- src/list/list.h
+++ src/list/list.h
@@ -1,62 +1,65 @@
 #ifndef CPP2_S21_LIST_LIST_H_
 #define CPP2_S21_LIST_LIST_H_
 
+#include <initializer_list>
+
 #include "list_iterators.h"
 
-namespace s21{
+namespace s21 {
 
 template <class T>
-class list{
-	public:
-		using value_type = T;
-		using reference = T &;
-		using const_reference = const T &;
-		using iterator = ListIterator<T>;
-		using const_iterator = ListConstIterator<T>;
-		using size_type = size_t;
+class list {
+ public:
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using iterator = ListIterator<T>;
+  using const_iterator = ListConstIterator<T>;
+  using size_type = size_t;
 
-		list();
-		list(size_type n);
-		list(std::initializer_list<value_type> const &items);
-		list(const list &l);
-		list(list &&l);
-		~list();
-		list &operator=(list &&l);
+  list();
+  list(size_type n);
+  list(std::initializer_list<value_type> const &items);
+  list(const list &l);
+  list(list &&l);
+  ~list();
+  list &operator=(list &&l);
 
-		const_reference front();
-		const_reference back();
+  const_reference front();
+  const_reference back();
 
-		bool empty();
-		size_type size();
-		size_type max_size();
+  bool empty();
+  size_type size();
+  size_type max_size();
 
-		void clear();
-		iterator insert(iteraror pos, const_reference value);
-		void erase(iterator pos);
-		void push_back(const_reference value);
-		void pop_back();
-		void push_front(const_reference value);
-		void pop_front();
-		void swap(list &other);
-		void merge(list &other);
-		void splice(const const_iterator pos, list &other);
-		void reverse();
-		void unique();
-		void sort();
+  void clear();
+  iterator insert(iteraror pos, const_reference value);
+  void erase(iterator pos);
+  void push_back(const_reference value);
+  void pop_back();
+  void push_front(const_reference value);
+  void pop_front();
+  void swap(list &other);
+  void merge(list &other);
+  void splice(const const_iterator pos, list &other);
+  void reverse();
+  void unique();
+  void sort();
 
-	private:
-	struct Node{
-		T data;
-		struct Node *next = nullptr;
-		struct Node *prior = nullptr;
-	};
-	Node *head_ = nullptr;
-	Node *tail_ = nullptr;
-	size_type size_ = 0;
+ private:
+  struct Node {
+    T data_;
+    struct Node *next_ = nullptr;
+    struct Node *prior_ = nullptr;
+
+    Node(value_type data, Node *next = nullptr, Node *prior = nullptr)
+        : data_(data), next_(next), prior_(prior) {}
+  };
+  size_type size_ = 0;
+  Node *head_ = nullptr;
+  Node *tail_ = nullptr;
 };
 
+}  // namespace s21
 
-
-} // namespace s21
-
-#endif // CPP2_S21_LIST_LIST_H_
+#endif  // CPP2_S21_LIST_LIST_H_
--- src/list/list.tpp
+++ src/list/list.tpp
@@ -1,1 +1,34 @@
 #include "list.h"
+
+namespace s21 {
+
+template <class T>
+list<T>::list() : size_(0), head_(nullptr), tail_(nullptr) {}
+
+template <class T>
+list<T>::list(size_type n) : list(){
+	for(size_type i = 0; i < n; ++i)
+		push_back(value_type());
+}
+
+template <class T>
+list<T>::list(std::initializer_list<value_type> const &items) : list(){
+	for( )
+}
+
+template <class T>
+list<T>::list(const list &l){
+
+}
+
+template <class T>
+list<T>::list(list &&l){
+
+}
+
+template <class T>
+list<T>::~list(){
+
+}
+
+}  // namespace s21
