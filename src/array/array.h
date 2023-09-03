efe9f89b8cc1a2ea24d1df79fe803e1546f5e17f
Sandor Merle <sandorme@ob-i5.nsk.21-school.ru>
2023-09-03 17:43:36 +0700

    array no max size

--- src/array/array.h
+++ src/array/array.h
@@ -1,18 +1,49 @@
 #ifndef CPP2_S21_ARRAY_ARRAY_H_
 #define CPP2_S21_ARRAY_ARRAY_H_
 
-namespace s21{
+namespace s21 {
 
-	template<class T>
-	struct array{
-		using value_type = T;
-		using reference = T &;
-		using const_reference = const T &;
-		using iterator = T *;
-		using const_iterator = const T *;
-		using size_type = size_t;
-	}
+template <class T, std::size_t N>
+struct array {
+  T data[N];
 
-} // namespace s21
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using iterator = T *;
+  using const_iterator = const T *;
+  using size_type = size_t;
 
-#endif // CPP2_S21_ARRAY_ARRAY_H_
+  reference at(size_type pos) {
+    if (pos >= N) throw std::out_of_range("out of range");
+    return data[pos];
+  }
+  reference operator[](size_type pos) { return data[pos]; }
+  const_reference operator[](size_type pos) const { return data[pos]; }
+
+  reference front() { return data[0]; }
+  const_reference front() const { return data[0]; }
+
+  reference back() { return data[N - 1]; }
+  const_reference back() const { return data[N - 1]; }
+
+  iterator data() noexcept { return data; }
+  const_iterator data() const noexcept { return data; }
+
+  iterator begin() noexcept { return data; }
+  const_iterator begin() const noexcept { return data; }
+
+  iterator end() { return data + N; }
+  const_iterator end() { return data + N; }
+
+  bool empty() const noexcept { return N == 0; }
+  size_type size() const noexcept { return N; }
+  size_type max_size() const noexcept { ; }
+
+  void swap(array &other) noexcept { std::swap(data, other.data); }
+  void fill(const_reference value) { std::fill(data, data + N - 1, value); }
+}
+
+}  // namespace s21
+
+#endif  // CPP2_S21_ARRAY_ARRAY_H_
