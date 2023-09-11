0bbcc21559509fdade8fd68fb41f0c8a0326ff3a
Takemiya Micheline <takemiym@ob-i3.nsk.21-school.ru>
2023-09-11 20:50:51 +0700

    Mon Sep 11 20:50:51 +07 2023

---
+++ src/set.h
@@ -0,0 +1,17 @@
+#ifndef S21_CONTAINERS_SRC_SET_H
+#define S21_CONTAINERS_SRC_SET_H
+
+#include "tree/rb_tree.h"
+
+template <class T> class set : public RBTree {
+public:
+  using key_type = T;
+  using value_type = key_type;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = BTreeIterator;
+
+  private:
+};
+
+#endif
