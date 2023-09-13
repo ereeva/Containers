d3e080fe39b37b35e9cdccf25ab063c8de594563
Takemiya Micheline <takemiym@student.21-school.ru>
2023-09-14 02:04:33 +0700

    Thu Sep 14 02:04:33 +07 2023

--- src/tree/test_set.cc
+++ src/tree/test_set.cc
@@ -4,9 +4,9 @@
 int main() {
   int a[3] = {1, 2, 3};
   s21::RBTree<int> tree;
-  tree.Insert(a[0]);
-  tree.Insert(a[1]);
   tree.Insert(a[2]);
+  tree.Insert(a[1]);
+  tree.Insert(a[0]);
   s21::RBTree<int>::iterator ptr = tree.begin();
   std::cout << *ptr++;
   std::cout << *ptr++;
