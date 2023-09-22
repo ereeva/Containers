1b0001ff14718bcfd75a53fb139498022a8af547
sandorme <sandorme@student.21-school.ru>
2023-09-22 15:08:30 +0700

    containers.h

---
+++ src/s21_containers.h
@@ -0,0 +1,9 @@
+#ifndef CPP2_S21_CONTAINERS_H_
+#define CPP2_S21_CONTAINERS_H_
+
+#include "list/list.h"
+#include "queue/queue.h"
+#include "stack/stack.h"
+#include "vector/vector.h"
+
+#endif // CPP2_S21_CONTAINERS_H_
